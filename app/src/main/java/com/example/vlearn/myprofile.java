624b7889d84d8a2c4ebe7c98a2ff0ad8b82b5f41
shubhamdamani <damani.shubham4@gmail.com>
2019-09-01 19:36:32 +0530

    Add existing file

--- app/src/main/java/com/example/vlearn/myprofile.java
+++ app/src/main/java/com/example/vlearn/myprofile.java
@@ -3,11 +3,11 @@ package com.example.vlearn;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class myprofile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_myprofile);
     }
 }
