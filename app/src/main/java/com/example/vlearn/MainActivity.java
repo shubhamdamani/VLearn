857fd29352f609913149812d1a47b5a96028cd4e
shubhamdamani <damani.shubham4@gmail.com>
2019-09-01 21:17:07 +0530

    Add existing file

--- app/src/main/java/com/example/vlearn/MainActivity.java
+++ app/src/main/java/com/example/vlearn/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.vlearn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.button_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment sel=null;
+                    switch(menuItem.getItemId()){
+                        case R.id.nav_news:
+                            sel=new NewsFragment();
+                            break;
+                        case R.id.nav_posts:
+                            sel=new PostsFragment();
+                            break;
+                        case R.id.nav_ques:
+                            sel=new QuesFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            sel).commit();
+                    return true;
+                }
+            };
 }
